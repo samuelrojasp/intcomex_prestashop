110f4fb05152c7186e78c6bd729217f4bac2ac0f
samuelrojasp <samuel.rojasp@gmail.com>
2021-06-30 10:38:43 -0400

    feat: template hereda estilos del admin

--- controllers/admin/AdminHomeController.php
+++ controllers/admin/AdminHomeController.php
@@ -7,6 +7,8 @@ class AdminHomeController extends FrameworkBundleAdminController
 {
     public function frontPage()
     {
-        return $this->render('@Modules/intcomeximport/views/admin/front.html.twig');
+        return $this->render('@Modules/intcomeximport/views/admin/front.html.twig', [
+            'enableSidebar' => true,
+        ]);
     }
 }
